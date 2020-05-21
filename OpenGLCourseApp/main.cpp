a999233ee1db63bb7921e0004946c5fe897bf90e
Noah Day <sheltielover432@gmail.com>
2020-05-20 22:45:46 -0400

    Scaling

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -10,9 +10,11 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 GLuint VBO, VAO, shader, uniformModel;
-bool direction = true;
 float triOffset = 0.0f, triMaxoffset = 0.7f, triIncrement = 0.065f;
+float curSize = 0.4f, maxSize = 0.8f, minSize = 0.1f;
 float curAngle = 0.0f;
+bool direction = true;
+bool sizeDirection = true;
 static const char* vShader = "                                                \n\
 #version 330                                                                  \n\
                                                                               \n\
@@ -22,18 +24,18 @@ uniform mat4 model;                                                           \n
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);		  \n\
+    gl_Position = model * vec4(pos, 1.0);									  \n\
 }";
 
 // Fragment Shader
 static const char* fShader = "                                                \n\
 #version 330                                                                  \n\
                                                                               \n\
-out vec4 colour;                                                               \n\
+out vec4 colour;                                                              \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    colour = vec4(1.0, 0.0, 0.0, 1.0);                                         \n\
+    colour = vec4(1.0, 0.0, 0.0, 1.0);                                        \n\
 }";
 
 void CreateTriangle()
@@ -86,7 +88,7 @@ void CompileShaders()
 	AddShader(shader, vShader, GL_VERTEX_SHADER);
 	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
 	GLint result = 0;
-	GLchar eLog[1024] = { 0 };
+	GLchar eLog[1024] = {0};
 	glLinkProgram(shader);
 	glGetProgramiv(shader, GL_LINK_STATUS, &result);
 	if (!result)
@@ -142,11 +144,11 @@ int main()
 	while (!glfwWindowShouldClose(mainWindow))
 	{
 		glfwPollEvents();
-		if (!direction)
+		if (direction)
 		{
 			triOffset += triIncrement;
 		}
-		else
+		else 
 		{
 			triOffset -= triIncrement;
 		}
@@ -154,17 +156,29 @@ int main()
 		{
 			direction = !direction;
 		}
-		curAngle += 0.001f;
+		curAngle += 0.01f;
 		if (curAngle >= 360)
 		{
 			curAngle -= 360;
+		}
+		if (direction)
+		{
+			curSize += 0.0001f;
+		}
+		else 
+		{
+			curSize -= 0.0001f;
+		}
+		if (curSize >= maxSize || curSize <= minSize)
+		{
+			sizeDirection = !sizeDirection;
 		}
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shader);
 		glm::mat4 model(1.0f);
 		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(curSize, 0.4f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
