a4c8b4cd4187dac4d393bab566e0fb5cae4f8ab8
Noah Day <sheltielover432@gmail.com>
2020-05-21 22:40:42 -0400

    Projections

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -9,7 +9,7 @@
 
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
-GLuint VBO, VAO, shader, IBO, uniformModel;
+GLuint VBO, VAO, shader, IBO, uniformModel, uniformProjection;
 float triOffset = 0.0f, triMaxoffset = 0.7f, triIncrement = 0.065f;
 float curSize = 0.4f, maxSize = 0.8f, minSize = 0.1f;
 float curAngle = 0.0f;
@@ -23,10 +23,11 @@ layout (location = 0) in vec3 pos;											  \n\
 out vec4 vCol;																  \n\
 																			  \n\
 uniform mat4 model;                                                           \n\
+uniform mat4 projection;                                                       \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = model * vec4(pos, 1.0);									  \n\
+    gl_Position = projection * model * vec4(pos, 1.0);						  \n\
 	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);								  \n\
 }";
 
@@ -121,6 +122,7 @@ void CompileShaders()
 		return;
 	}
 	uniformModel = glGetUniformLocation(shader, "model");
+	uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 int main()
@@ -157,6 +159,7 @@ int main()
 	glViewport(0, 0, bufferWidth, bufferHeight);
 	CreateTriangle();
 	CompileShaders();
+	glm::mat4 projection = glm::perspective(45.0f,(GLfloat)bufferWidth/(GLfloat)bufferHeight, 0.1f, 100.0f);
 	while (!glfwWindowShouldClose(mainWindow))
 	{
 		glfwPollEvents();
@@ -193,9 +196,11 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(shader);
 		glm::mat4 model(1.0f);
-		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model,curAngle*toRadians, glm::vec3(0.4f, 0.4f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 		glBindVertexArray(VAO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
