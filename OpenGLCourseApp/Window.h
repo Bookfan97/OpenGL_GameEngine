dd886257632c4b6bd8c37588f9c3e062425fbc61
Noah Day <sheltielover432@gmail.com>
2020-05-22 20:42:19 -0400

    Camera: Input with GLFW

--- OpenGLCourseApp/Window.h
+++ OpenGLCourseApp/Window.h
@@ -13,10 +13,19 @@ public:
 	GLint getBufferWidth() { return bufferWidth; }
 	GLint getBufferHeight() { return bufferHeight; }
 	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+	bool* getsKeys() { return keys; }
+	GLfloat getXChange();
+	GLfloat getYChange();
 	void swapBuffers() { glfwSwapBuffers(mainWindow); }
 	~Window();
 private:
 	GLFWwindow* mainWindow;
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+	bool keys[1024];
+	GLfloat lastX, lastY, xChange, yChange;
+	bool mouseFirstMoved;
+	void createCallbacks();
+	static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+	static void handleMouse(GLFWwindow* window, double xPos, double yPos);
 };
